60be97d06bb583025c05e172c2db3ab2dbae58ad
Raveen <raveenrv92@gmail.com>
2024-10-08 17:04:47 +0530

    Added authentication using clerk, base layout, database setup, wizard page, dashboard page-intro, dashboard page-category picker

---
+++ app/(dashboard)/_actions/transactions.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import {
+  CreateTransactionSchema,
+  CreateTransactionSchemaType,
+} from "@/schema/transaction";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export const CreateTransaction = async (form: CreateTransactionSchemaType) => {
+  const parsedBody = CreateTransactionSchema.safeParse(form);
+
+  if (!parsedBody.success) {
+    throw new Error(parsedBody.error.message);
+  }
+
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  const { amount, category, date, description, type } = parsedBody.data;
+
+  const categoryRow = await prisma.category.findFirst({
+    where: {
+      userId: user.id,
+      name: category,
+    },
+  });
+
+  if (!categoryRow) {
+    throw new Error("Category not found");
+  }
+
+  //   NOTE: Don't make confussion between $transaction (prisma) and prisma.transaction (table)
+
+  await prisma.$transaction([
+    // Create user transaction
+
+    prisma.transaction.create({
+      data: {
+        userId: user.id,
+        amount,
+        date,
+        description: description || "",
+        type,
+        category: categoryRow.name,
+        categoryIcon: categoryRow.icon,
+      },
+    }),
+
+    // Update aggregate table
+  ]);
+};
